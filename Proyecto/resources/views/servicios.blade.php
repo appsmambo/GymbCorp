09d4801cd7e860c44c8e80ac421f68e1198152e5
Juan Quintanilla <quintanilla.peru@gmail.com>
2017-08-22 22:58:07 -0500

    servicios

---
+++ Proyecto/resources/views/servicios.blade.php
@@ -0,0 +1,114 @@
+@extends("layouts/main")
+@section('contenido')
+      <section class="container no-padding">
+        <h3>
+          SERVICIOS
+        </h3>
+      </section>
+      <section class="container no-padding">
+        <div class="row contenido">
+          <div class="col-md-10 col-md-offset-1">
+            <div class="row">
+              <div class="col-md-3">
+                <p class="text-center servicio wow zoomIn" data-wow-delay=".5s">
+                  <img src="{{url('img/servicio-1.png')}}" class="img-responsive center-block" alt="">
+                  <br>
+                  Asesoría y Servicio<br>Tributario
+                  <br>
+                  <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+                  <a href="#" class="leer-mas">
+                    Leer más
+                  </a>
+                </p>
+              </div>
+              <div class="col-md-3">
+                <p class="text-center servicio wow zoomIn" data-wow-delay=".5s">
+                  <img src="{{url('img/servicio-2.png')}}" class="img-responsive center-block" alt="">
+                  <br>
+                  Asesoría y Servicio<br>Financiero
+                  <br>
+                  <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+                  <a href="#" class="leer-mas">
+                    Leer más
+                  </a>
+                </p>
+              </div>
+              <div class="col-md-3">
+                <p class="text-center servicio wow zoomIn" data-wow-delay=".5s">
+                  <img src="{{url('img/servicio-3.png')}}" class="img-responsive center-block" alt="">
+                  <br>
+                  Asesoría y Servicio<br>Contable
+                  <br>
+                  <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+                  <a href="#" class="leer-mas">
+                    Leer más
+                  </a>
+                </p>
+              </div>
+              <div class="col-md-3">
+                <p class="text-center servicio wow zoomIn" data-wow-delay=".5s">
+                  <img src="{{url('img/servicio-4.png')}}" class="img-responsive center-block" alt="">
+                  <br>
+                  Administración de<br>Recursos Humanos
+                  <br>
+                  <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+                  <a href="#" class="leer-mas">
+                    Leer más
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-3">
+                <p class="text-center servicio wow zoomIn" data-wow-delay=".5s">
+                  <img src="{{url('img/servicio-5.png')}}" class="img-responsive center-block" alt="">
+                  <br>
+                  Toma de<br>Inventarios
+                  <br>
+                  <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+                  <a href="#" class="leer-mas">
+                    Leer más
+                  </a>
+                </p>
+              </div>
+              <div class="col-md-3">
+                <p class="text-center servicio wow zoomIn" data-wow-delay=".5s">
+                  <img src="{{url('img/servicio-6.png')}}" class="img-responsive center-block" alt="">
+                  <br>
+                  Consultoría<br>Empresarial
+                  <br>
+                  <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+                  <a href="#" class="leer-mas">
+                    Leer más
+                  </a>
+                </p>
+              </div>
+              <div class="col-md-3">
+                <p class="text-center servicio wow zoomIn" data-wow-delay=".5s">
+                  <img src="{{url('img/servicio-7.png')}}" class="img-responsive center-block" alt="">
+                  <br>
+                  Seguridad y Salud<br>en el Trabajo
+                  <br>
+                  <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+                  <a href="#" class="leer-mas">
+                    Leer más
+                  </a>
+                </p>
+              </div>
+              <div class="col-md-3">
+                <p class="text-center servicio wow zoomIn" data-wow-delay=".5s">
+                  <img src="{{url('img/servicio-8.png')}}" class="img-responsive center-block" alt="">
+                  <br>
+                  Proyectos<br>Factibilidad
+                  <br>
+                  <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+                  <a href="#" class="leer-mas">
+                    Leer más
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+@stop
--- Proyecto/routes/web.php
+++ Proyecto/routes/web.php
@@ -3,3 +3,39 @@
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('servicios', function () {
+    return view('servicios');
+});
+
+Route::get('servicios/asesoria-y-servicio-tributario', function () {
+    return view('servicios/asesoria-y-servicio-tributario');
+});
+
+Route::get('/servicios/asesoria-y-servicio-financiero', function () {
+    return view('servicios/asesoria-y-servicio-financiero');
+});
+
+Route::get('/servicios/asesoria-y-servicio-contable', function () {
+    return view('servicios/asesoria-y-servicio-contable');
+});
+
+Route::get('/servicios/administracion-de-recursos-humanos', function () {
+    return view('servicios/administracion-de-recursos-humanos');
+});
+
+Route::get('/servicios/toma-de-inventarios', function () {
+    return view('servicios/toma-de-inventarios');
+});
+
+Route::get('/servicios/consultoria-empresarial', function () {
+    return view('servicios/consultoria-empresarial');
+});
+
+Route::get('/servicios/seguridad-y-salud-en-el-trabajo', function () {
+    return view('servicios/seguridad-y-salud-en-el-trabajo');
+});
+
+Route::get('/servicios/proyectos-factibilidad', function () {
+    return view('servicios/proyectos-factibilidad');
+});
